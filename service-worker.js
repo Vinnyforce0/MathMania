e098e76a3f59a1f53afcb405856f96dcb183d4ca
vinnyforce0 <jorgevinny2008@gmail.com>
2026-02-26 20:25:39 -0300

    vai dar certo nunca isso aq mds

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'MathMania';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/CSS/styles.css',
+  '/HTML/indice.html',
+  '/HTML/iniciar.html',
+  '/HTML/opcoes.html',
+  '/SRC/play.js',
+  '/IMAGES/floor.jpg',
+  '/IMAGES/laser.png',
+  '/IMAGES/sky.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
