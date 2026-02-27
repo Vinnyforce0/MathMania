2a42142ca70460d9ce84880581832d41c285daa8
vinnyforce0 <jorgevinny2008@gmail.com>
2026-02-26 22:39:36 -0300

    uuuuh

--- SRC/config.js
+++ SRC/config.js
@@ -2,7 +2,7 @@
 // CONFIGURAÇÃO GLOBAL
 // =======================
 
-const APP_VERSION = "1.0.6";
+const APP_VERSION = "1.0.7"; // Atualize esta versão a cada mudança significativa
 
 const versionupdate = document.getElementById('appVersion');
 versionupdate.textContent = `Versão: ${APP_VERSION}`;
