e88af20b7f5cb0756623e989b6cda2c9586f1ff8
vinnyforce0 <jorgevinny2008@gmail.com>
2026-02-26 22:36:18 -0300

    mais update desnecessario

--- SRC/config.js
+++ SRC/config.js
@@ -2,4 +2,7 @@
 // CONFIGURAÇÃO GLOBAL
 // =======================
 
-const APP_VERSION = "1.0.5";
+const APP_VERSION = "1.0.6";
+
+const versionupdate = document.getElementById('appVersion');
+versionupdate.textContent = `Versão: ${APP_VERSION}`;
