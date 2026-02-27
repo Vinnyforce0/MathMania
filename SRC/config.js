f0a9a11933b2fb49f4082da00a4ffc225e05ace5
vinnyforce0 <jorgevinny2008@gmail.com>
2026-02-26 22:45:17 -0300

    funciona por favor

--- SRC/config.js
+++ SRC/config.js
@@ -2,7 +2,7 @@
 // CONFIGURAÇÃO GLOBAL
 // =======================
 
-const APP_VERSION = "1.0.7"; // Atualize esta versão a cada mudança significativa
+const APP_VERSION = "1.0.10"; // Atualize esta versão a cada mudança significativa
 
 const versionupdate = document.getElementById('appVersion');
 versionupdate.textContent = `Versão: ${APP_VERSION}`;
