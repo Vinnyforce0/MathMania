1dc2c699c9813b8d1994576db7cc6a210ea7acf5
vinnyforce0 <jorgevinny2008@gmail.com>
2026-02-26 22:50:03 -0300

    desisto completamente

--- SRC/config.js
+++ SRC/config.js
@@ -2,7 +2,4 @@
 // CONFIGURAÇÃO GLOBAL
 // =======================
 
-const APP_VERSION = "1.0.10"; // Atualize esta versão a cada mudança significativa
-
-const versionupdate = document.getElementById('appVersion');
-versionupdate.textContent = `Versão: ${APP_VERSION}`;
+const APP_VERSION = "1.0.11"; // Atualize esta versão a cada mudança significativa
