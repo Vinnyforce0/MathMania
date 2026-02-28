c1a82a91eb17864fbad99e5ea42860853189fb7a
vinnyforce0 <jorgevinny2008@gmail.com>
2026-02-28 14:42:12 -0300

    mais uma tent

--- SRC/config.js
+++ SRC/config.js
@@ -2,4 +2,4 @@
 // CONFIGURAÇÃO GLOBAL
 // =======================
 
-const APP_VERSION = "1.2.2"; // Atualize esta versão a cada mudança significativa
+const APP_VERSION = "1.2.3"; // Atualize esta versão a cada mudança significativa
